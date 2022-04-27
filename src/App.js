2f6d70bb219b9e562fab7588fa52ab10b01e7bfb
Sinh <hadangsinh1911@gmail.com>
2022-04-27 15:20:05 +0700

    add code

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import {
   Route,
   Link,
 } from "react-router-dom";
+import Home from './components/Home';
+import Chat from './components/Chat';
 import Login from './components/Login';
 import Signup from './components/Signup';
 export default function App() {
@@ -12,13 +14,10 @@ export default function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/signup" element={<Signup />} >
-        
-          </Route>
-  
-          <Route path="/login" element={<Login />} >
-        
-          </Route>
+        <Route path="/home" element={<Home />} />
+        <Route path="/chat" element={<Chat />} />
+          <Route path="/signup" element={<Signup />} ></Route>
+          <Route path="/" element={<Login />} ></Route>
         </Routes>
         {/* <Link to={`/about?name=mien&age=20`}>about</Link> */}
       </BrowserRouter>
