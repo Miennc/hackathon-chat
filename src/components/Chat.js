4cd71e744e6f57a1c88b5a50bf132e7f9fc81bbd
Mien <2502mien@gmail.com>
2022-04-28 09:16:26 +0700

    add

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { db } from "../firebase";
 import { useSearchParams } from 'react-router-dom';
-import { collection, getDocs, deleteDoc, doc, onSnapshot, getDoc, query, where, addDoc, orderBy, limit,} from 'firebase/firestore';
+import { collection, getDocs, deleteDoc, doc, onSnapshot, getDoc, query, where, addDoc, orderBy, limit, } from 'firebase/firestore';
 function Chat(props) {
     const [message, setMessage] = useState('');
     const [searchParams] = useSearchParams();
@@ -27,8 +27,8 @@ function Chat(props) {
     useEffect(() => {
         (async () => {
             const collectionRef = collection(db, 'chat');
-             const collectionQuery = query(collectionRef, where('userUid', 'in', [[userId, user.uid], [user.uid, userId]]));
-                    
+            const collectionQuery = query(collectionRef, where('userUid', 'in', [[userId, user.uid], [user.uid, userId]]));
+
             unSub = onSnapshot(collectionQuery, (snapShot) => {
                 const localMessage = [];
                 snapShot.forEach(doc => {
@@ -46,13 +46,13 @@ function Chat(props) {
 
     }, []);
 
-    // const DeleteNote = async (id) => {
+    // const deleteNote = async (id) => {
     //     const docRef = doc(db, 'chat', id);
     //     await deleteDoc(docRef)
     // }
     return (
         <div>
-            <div className=" bg-yellow-300 ">
+            <div className=" bg-green-300">
                 <div className="w-10/12 bg-white mx-auto">
                     <div className="flex-1 p:2 sm:p-6 justify-between flex flex-col h-screen">
                         <div className="flex sm:items-center justify-between py-3 border-b-2 border-gray-200">
@@ -79,7 +79,7 @@ function Chat(props) {
                                     className="inline-flex items-center justify-center rounded-lg border h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                         stroke="currentColor" className="h-6 w-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                        <path stroke-linecap="round" stroke-linejoin="round"
                                             d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
                                     </svg>
                                 </button>
@@ -111,7 +111,7 @@ function Chat(props) {
                                                 <div className={user.uid === messageItem.uid ? "flex items-end justify-end" : "flex items-end"}>
                                                     <div className={user.uid === messageItem.uid ? "flex flex-col space-y-2 text-xs order-1 max-w-xs mx-2 items-end" : "flex flex-col space-y-2 text-xs order-2 max-w-xs mx-2 items-end"}>
                                                         <div><span
-                                                            className={user.uid === messageItem.uid ? "px-4 py-2 rounded-lg inline-block rounded-br-none bg-blue-600 text-white  text-xl" : "px-4 py-2 rounded-lg inline-block rounded-br-none bg-pink-600 text-white "}>{messageItem.message}</span>
+                                                            className={user.uid === messageItem.uid ? "px-4 py-2 rounded-lg inline-block rounded-br-none bg-blue-600 text-white  text-xl" : "px-4 py-2 rounded-lg inline-block rounded-br-none bg-pink-600 text-white text-xl"}>{messageItem.message} </span>
                                                         </div>
                                                     </div>
                                                     <img
@@ -126,15 +126,15 @@ function Chat(props) {
 
 
                         </div>
-                      
-                     <form onSubmit={postMessage}>
-                         <input type="file" />
-                         <input type="text" value={message} onChange={(e)=>setMessage(e.target.value)} placeholder="Aa"  className="border-2 border-green-400 w-96 p-2"/>
-                     </form>
+
+                        <form onSubmit={postMessage}>
+                            <input type="file" />
+                            <input type="text" value={message} onChange={(e) => setMessage(e.target.value)} placeholder="Aa" className="border-2 border-green-400 w-96 p-2" />
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
-            </div >
+        </div >
     );
 }
 
